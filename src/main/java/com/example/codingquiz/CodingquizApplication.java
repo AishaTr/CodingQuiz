c927e54eda9c9a36ac3924830a96b47101773611
Aisha Thermidor <aishathermidor@Aishas-MacBook-Pro.local>
2019-12-04 09:12:25 -0500

    Initial commit

---
+++ src/main/java/com/example/codingquiz/CodingquizApplication.java
@@ -0,0 +1,13 @@
+package com.example.codingquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingquizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingquizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/codingquiz/CodingquizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codingquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingquizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
